b215e69f92721a52e4be4f6338533a836660fc49
Karolpz <carolinelpz34@gmail.com>
2025-09-16 14:59:54 +0200

    test again

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './App.css'
 import Router from './Router'
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
+import Home from './pages/Home'
 
 function App() {
 
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <main>
         <Router />
       </main>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
 
   )
 }
